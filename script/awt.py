0958a8a27b1af12d0e32f3e2f5e8cc3f1adf4cd5
Nikita Prokopov <niki@tonsky.me>
2022-11-20 14:37:44 +0100

    awt options

--- script/awt.py
+++ script/awt.py
@@ -18,7 +18,25 @@ def mkdirs(path):
 
 def main(argv):
   subprocess.check_call(["javac", "-encoding", "UTF8", "--release", "11", "-d", "classes"] + glob.glob("src/awt/*.java"), cwd=WD)
-  subprocess.check_call(["java", "-cp", "classes", "-ea", "awt.Main"], cwd=WD)
+  subprocess.check_call([
+    "java", 
+    "-cp", "classes",
+    "-ea",
+    "--illegal-access=warn",
+    "--add-opens=java.desktop/java.awt.event=ALL-UNNAMED",
+    "--add-opens=java.desktop/java.awt=ALL-UNNAMED",
+    "--add-opens=java.desktop/sun.awt=ALL-UNNAMED",
+    "--add-opens=java.base/java.lang=ALL-UNNAMED",
+    "--add-opens=java.base/java.util=ALL-UNNAMED",
+    "--add-opens=java.desktop/javax.swing=ALL-UNNAMED",
+    "--add-opens=java.desktop/sun.swing=ALL-UNNAMED",
+    "--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED",
+    "--add-opens=java.desktop/java.awt.peer=ALL-UNNAMED",
+    "--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED",
+    "--add-exports=java.desktop/sun.font=ALL-UNNAMED",
+    "--add-exports=java.desktop/com.apple.eawt=ALL-UNNAMED",
+    "--add-exports=java.desktop/com.apple.laf=ALL-UNNAMED",
+    "awt.Main"], cwd=WD)
   return 0
 
 if __name__ == '__main__':
